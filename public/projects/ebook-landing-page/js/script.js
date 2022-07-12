4a4251858c9018ae94e1ad7d302cddaa6d4b39ee
harmyportfolio <harmyportfolio@protonmail.com>
2022-07-12 16:02:12 +0100

    Init

---
+++ public/projects/ebook-landing-page/js/script.js
@@ -0,0 +1,27 @@
+var countDate = new Date('November 5, 2022 00:00:00').getTime();
+
+const newYear = () => {
+    var now = new Date().getTime();
+    gap = countDate - now;
+
+    var second = 1000;
+    var minute = second * 60;
+    var hour = minute * 60;
+    var day = hour * 24;
+
+    var d = Math.floor(gap / (day));
+    var h = Math.floor((gap % (day)) / (hour));
+    var m = Math.floor((gap % (hour)) / (minute));
+    var s = Math.floor((gap % (minute)) / (second));
+
+    document.getElementById('day').innerHTML = d;
+    document.getElementById('hour').innerHTML = h;
+    document.getElementById('minute').innerHTML = m;
+    document.getElementById('second').innerHTML = s;
+
+}
+
+setInterval(() => {
+    newYear();
+}, 1000);
+
